a9022aafbe8470ea35c10e2389f17f78508cfbba
Norberto Santiago <norbertosantiago82@gmail.com>
2020-04-03 20:47:47 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,7 @@
 class Actor < ActiveRecord::Base
   
   has_many :characters
-  has_many :shows, through: :characters
+  has_many :shows, through: :character
   
   def full_name
     name = "#{self.first_name} #{self.last_name}"
